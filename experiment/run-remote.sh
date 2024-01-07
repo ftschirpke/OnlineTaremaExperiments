cc225c82cd957d05ad8a3fa1c71e014d019394d2
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2024-01-07 14:54:53 +0100

    feat: inital setup

---
+++ experiment/run-remote.sh
@@ -0,0 +1,24 @@
+cd $(dirname $0)
+
+export KUBECONFIG="secrets/kubeconfig.yaml"
+namespace=ftschirpke
+
+# setup remote cluster
+kubectl apply -f cluster/accounts.yaml -n $namespace
+kubectl apply -f cluster/pvc.yaml -n $namespace
+kubectl apply -f cluster/nextflow-pod.yaml -n $namespace
+kubectl wait --for=condition=ready pod nextflow -n $namespace
+kubectl exec -n $namespace nextflow -- bash -c "apk add tar rsync openssh sshpass"
+kubectl exec -n $namespace nextflow -- bash -c "cd /usr/local/bin/ && curl -LO 'https://dl.k8s.io/release/v1.28.3/bin/linux/amd64/kubectl' && chmod 700 kubectl"
+
+# upload experiment data and scripts to remote cluster
+for entry in run.sh nextflow.config configs/ cluster/
+do
+    echo "Copying $entry to remote cluster"
+    kubectl cp -n $namespace ./$entry $namespace/nextflow:/experiments/experiment/$entry
+done
+
+results_dir=/experiments/bachelor_results
+logname=$(date +%Y%m%d-%H%M%S).log
+# run experiment on remote cluster
+kubectl exec -n $namespace nextflow -- bash -c "mkdir -p $results_dir && nohup bash run.sh &> $results_dir/$logname & echo 'Successfully started remote experiment'"
---
+++ experiment/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# remove old data
+rm -rf /input/data
+
+# load to cache  TODO: uncomment this and provide a loadToCache.yaml for each workflow
+# echo "workflow: $workflow: loading to cache"
+# kubectl apply -f $workflow/loadToCache.yaml -n $namespace
+# kubectl rollout status daemonset workflow-prepare -n $namespace
+# kubectl delete -f $workflow/loadToCache.yaml --wait -n $namespace
+
+results_dir=/experiments/bachelor_results
+experiment_name=test
+namespace=ftschirpke
+
+echo "workflow: $experiment_name - starting scheduler"
+kubectl apply -f cluster/workflow-scheduler.yaml --wait -n $namespace
+kubectl wait --for=condition=ready pod workflow-scheduler -n $namespace
+mkdir -p $results_dir/$experiment_name
+nohup kubectl logs workflow-scheduler -f -n $namespace > $results_dir/$experiment_name/scheduler.log &
+
+nextflow run hello
