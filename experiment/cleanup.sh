4b15cb8788138ded6a42cacf272cd60cb7021615
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2024-02-05 02:17:36 +0100

    chore: update version and cleaning scripts

--- experiment/cleanup.sh
+++ experiment/cleanup.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-export KUBECONFIG="secrets/kubeconfig.yaml"
-namespace=ftschirpke
-
-kubectl exec -n ftschirpke nextflow -- bash -c "rm -rd /experiments/experiment/*"
-kubectl exec -n ftschirpke nextflow -- bash -c "rm -rd /experiments/bachelor_results/test"
-
-kubectl delete ds -l app=nextflow --wait
-kubectl delete pods -l app=nextflow --wait
-kubectl delete pods -l nextflow.io/app=nextflow --wait
-
+bash stop_pods.sh
+bash delete_data.sh
